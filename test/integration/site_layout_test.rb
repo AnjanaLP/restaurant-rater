58c38e8d5d56517d1043f931c0de642125815892
Anjana <anjana.lpn@gmail.com>
2019-09-17 16:43:30 +0100

    update site layout test for logged in users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,5 +14,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", new_restaurant_path
     get signup_path
     assert_select "title", full_title("Sign up")
+    user = users(:anj)
+    log_in_as(user)
+    get root_path
+    assert_select "a[href=?]", signup_path, count: 0
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
+    assert_select "a[href=?]", logout_path
   end
 end
