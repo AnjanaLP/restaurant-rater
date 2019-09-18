c28d385d1ae97fe13c66b9782c7a5a0f76df0d16
Anjana <anjana.lpn@gmail.com>
2019-09-18 13:42:53 +0100

    chnage access control for user profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
     @reviews = @user.reviews
   end
 
